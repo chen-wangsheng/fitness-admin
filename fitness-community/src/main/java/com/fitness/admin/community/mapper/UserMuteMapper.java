ef926fb4366a8249d111fcef05a55477bf7e5929
chen-wangsheng <chen_wangsheng@sina.com>
2026-06-14 09:06:46 +0800

    完善社区功能

--- fitness-community/src/main/java/com/fitness/admin/community/mapper/UserMuteMapper.java
+++ fitness-community/src/main/java/com/fitness/admin/community/mapper/UserMuteMapper.java
@@ -2,6 +2,8 @@ package com.fitness.admin.community.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.fitness.admin.community.entity.UserMute;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMuteMapper extends BaseMapper<UserMute> {
 }
